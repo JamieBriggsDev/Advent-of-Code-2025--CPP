e51cfbdfd6ceb78290d3cfb0d72cd3024249fe71
Jamie Briggs <personal@jbriggs.dev>
2025-12-03 23:54:29 +0000

    Add ifdef for linux operating systems

--- cpp/day3/BatteryBank.h
+++ cpp/day3/BatteryBank.h
@@ -6,16 +6,19 @@
 #define AOC2025_BATTERY_H
 #include <vector>
 
+#ifdef __linux__
+#include <cstdint>
+#endif
+
 namespace D3 {
 
   class BatteryBank {
     std::vector<int8_t> batteries_;
     int8_t findIndexOfLargestNumberFromRange(int8_t startIdx, int8_t endIdx) const;
+
   public:
     BatteryBank(std::string batteries);
-    [[nodiscard]] std::vector<int8_t> &getBatteries() {
-      return batteries_;
-    };
+    [[nodiscard]] std::vector<int8_t> &getBatteries() { return batteries_; };
     long long getLargestJoltage() const;
     long long getLargestJoltage(int8_t totalBatteries) const;
   };
